0455db353faa03654089164d1651f12677d78d35
David Arena <david.andrew.arena@gmail.com>
2025-02-04 23:58:20 +0100

    Fixes

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import TradingSignal from "./components/TradingSignal"
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center p-24 bg-gray-100 text-black">
-      <h1 className="text-4xl font-bold mb-8">Trading Signal SPA</h1>
+      <h1 className="text-4xl font-bold mb-8">Mischa Says....</h1>
       <TradingSignal />
-      <p className="mt-8 text-xl">If you can see this, the app is working!</p>
+      <p className="mt-8 text-xl">Dont be late to the party!</p>
     </div>
   )
 }
